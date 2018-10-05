9e4f184b57da38d2760a967a240475a61e82be26
S Sergey <sergey@7smarts.com>
2018-10-05 17:58:41 +0400

    update db

---
+++ DataAccess/Migrations/20181005135758_second.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Tours",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Tours",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MoreInfo",
+                table: "Tours",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Tours",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Review",
+                table: "Tours",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCustomerId",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CityId",
+                table: "Customers",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_UserId",
+                table: "Customers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_City_CityId",
+                table: "Customers",
+                column: "CityId",
+                principalTable: "City",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Users_UserId",
+                table: "Customers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_City_CityId",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Users_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "City");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_CityId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Tours");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Tours");
+
+            migrationBuilder.DropColumn(
+                name: "MoreInfo",
+                table: "Tours");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Tours");
+
+            migrationBuilder.DropColumn(
+                name: "Review",
+                table: "Tours");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCustomerId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Customers");
+        }
+    }
+}
--- DataAccess/Models/Tour.cs
+++ DataAccess/Models/Tour.cs
@@ -7,5 +7,10 @@ namespace DataAccess.Models
     public class Tour
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double? Review { get; set; }
+        public string MoreInfo { get; set; }
     }
 }
