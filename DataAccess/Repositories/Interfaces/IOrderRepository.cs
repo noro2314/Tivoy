72d51df954555b6210ff0b5992c913736fa8ce6b
Norayr Gharagyozyan <noro2314@gmail.com>
2018-10-06 02:08:05 +0400

    orderss

--- DataAccess/Repositories/Interfaces/IOrderRepository.cs
+++ DataAccess/Repositories/Interfaces/IOrderRepository.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using X.PagedList;
+
 namespace DataAccess.Repositories.Interfaces
 {
    public interface IOrderRepository
     {
         Task Add(OrderViewModel model);
         Task<List<OrderViewModel>> GetAll();
+        Task<IPagedList> GetAllPagedList(int pageNumber, int pageSize, string searchstring = null);
     }
 }
