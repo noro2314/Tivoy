7e45026d0f12fccd02dbaa84d7dabec596ae2887
Rubo <rubo.haxinyan96@gmail.com>
2018-10-05 21:06:59 +0400

    identity

---
+++ Tivoy/Models/ViewModels/RegisterModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tivoy.Models.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+ 
+        [Required]
+        [Display(Name = "Date of birth")]
+        public DateTimeOffset Year { get; set; }
+ 
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+ 
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
